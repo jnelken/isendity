88e537497650eef6508c9a44599e49293d78d0a7
Jake Nelken <jnelks@gmail.com>
2017-05-13 02:58:54 -0400

    Get db running locally.

--- app.js
+++ app.js
@@ -50,4 +50,32 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+/////////////////////////////////////////////////////////
+
+var mysql      = require('mysql');
+var conf       = require('./conf.js');
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : conf.db.user,
+  password : conf.db.pass,
+  database : 'isendity'
+});
+// var connection = mysql.createConnection({
+//   host     : 'hngomrlb3vfq3jcr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//   user     : 'x55oi5an4p865jz1',
+//   password : 'a3bcbvkfz71lf40d',
+//   database : 'isendity'
+// });
+
+connection.connect();
+
+connection.query('SELECT * from tags', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.', err);
+});
+
+connection.end();
+
 module.exports = app;
